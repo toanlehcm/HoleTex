c9607723f50a11a6e65f11fec7caeff263bba677
Toan Le <thanhtoanhcm1602@gmail.com>
2024-06-11 11:51:56 +0700

    - CSS grid.

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import ExampleFunctional from './conponents/ExampleFunctional';
 import LessonHTMLAndCSS from './lesson/htmlcss';
+import LessonCSSGrid from './lesson/cssGrid';
 
 function App() {
   return (
     <div className="App">
       {/* <ExampleFunctional /> */}
-      <LessonHTMLAndCSS />
+      {/* <LessonHTMLAndCSS /> */}
+      <LessonCSSGrid />
     </div>
   );
 }
---
+++ src/lesson/cssGrid/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import styles from './styles.module.scss'
+
+function LessonCSSGrid(props) {
+  return (
+    <div>
+      <div className={styles.container} >
+        <div className={`${styles.item} ${styles.item_1}`} >item_1</div>
+        <div className={`${styles.item} ${styles.item_2}`} >item_2</div>
+        <div className={`${styles.item} ${styles.item_3}`} >item_3</div>
+        <div className={`${styles.item} ${styles.item_4}`} >item_4</div>
+        <div className={`${styles.item} ${styles.item_5}`} >item_5</div>
+      </div>
+    </div>
+  );
+}
+
+export default LessonCSSGrid;

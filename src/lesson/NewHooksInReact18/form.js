d59dff4a8d948fe99969009cdc210cd8a5afb8b9
Toan Le <thanhtoanhcm1602@gmail.com>
2024-06-14 02:25:46 +0700

    The new hooks 18: useDeferredValue.

--- src/lesson/NewHooksInReact18/form.js
+++ src/lesson/NewHooksInReact18/form.js
@@ -1,36 +1,56 @@
-import React, { useId, useMemo, useState, useTransition } from 'react';
+import React, { useId, useMemo, useState, useTransition, useDeferredValue } from 'react';
 import StudentList from './StudentList';
 import studentListData from '../../mocks/studentList.json';
 
 function FormComponent() {
   const id = useId()
   const [searchInput, setSearchInput] = useState('')
-  const [filterText, setFilterText] = useState('')
-  const [isPending, startTransition] = useTransition()
+  // const [filterText, setFilterText] = useState('')
+  // const [isPending, startTransition] = useTransition()
 
+  /*------------ useTransition --------------*/
   // const data = studentListData
+  // const data = useMemo(() => {
+  //   return studentListData.map((student) => {
+  //     const index = student.indexOf(filterText);
+
+  //     return index === -1
+  //       ? (<p>{student}</p>) // search not find.
+  //       : (
+  //         <p>
+  //           {student.slice(0, index)}
+  //           <span style={{ backgroundColor: 'yellow' }}> {student.slice(index, index + filterText.length)} </ span>
+  //           {student.slice(index + filterText.length)}
+  //         </p>
+  //       );
+  //   });
+  // }, [filterText]);
+
+  /*------------ useDeferredValue --------------*/
+  const deferedValue = useDeferredValue(searchInput)
+
   const data = useMemo(() => {
     return studentListData.map((student) => {
-      const index = student.indexOf(filterText);
+      const index = student.indexOf(deferedValue);
 
       return index === -1
         ? (<p>{student}</p>) // search not find.
         : (
           <p>
             {student.slice(0, index)}
-            <span style={{ backgroundColor: 'yellow' }}> {student.slice(index, index + filterText.length)} </ span>
-            {student.slice(index + filterText.length)}
+            <span style={{ backgroundColor: 'yellow' }}> {student.slice(index, index + deferedValue.length)} </ span>
+            {student.slice(index + deferedValue.length)}
           </p>
         );
     });
-  }, [filterText]);
+  }, [deferedValue]);
 
   const handleSearchInputChange = (e) => {
     setSearchInput(e.target.value)
 
-    startTransition(() => {
-      setFilterText(e.target.value)
-    })
+    // startTransition(() => {
+    //   setFilterText(e.target.value)
+    // })
   }
 
   return (
@@ -39,7 +59,8 @@ function FormComponent() {
       <label htmlFor={id}>Search name: </label>
       <input id={id} type='text' name='name' value={searchInput} onChange={handleSearchInputChange} />
 
-      {isPending ? <p>Loading...</p> : <StudentList data={data} />}
+      {/* {isPending ? <p>Loading...</p> : <StudentList data={data} />} */}
+      <StudentList data={data} />
     </div>
   );
 }

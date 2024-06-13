0526963e1e3c7661d2739afd0b4fd9e72b0a9931
Toan Le <thanhtoanhcm1602@gmail.com>
2024-06-13 17:36:20 +0700

    - UseRef: Query to DOM

--- src/lesson/LearnUseRef/index.js
+++ src/lesson/LearnUseRef/index.js
@@ -2,12 +2,12 @@ import React, { useEffect, useRef, useState } from 'react';
 
 function LearnUseRef(props) {
   const [count, setCount] = useState(0)
-  const countRef = useRef(0)
+  // const countRef = useRef(0)
   // const obj = { current: 0 }
 
   const handleClick = () => {
     // setCount(count + 1);
-    countRef.current = countRef.current + 1;
+    // countRef.current = countRef.current + 1;
     // obj.current = obj.current + 1;
   }
 
@@ -21,11 +21,21 @@ function LearnUseRef(props) {
   //   }, 1000)
   // }, [])
 
+  const ref = useRef(null)
+
+  useEffect(() => {
+    ref.current.focus()
+  }, [])
+
+  console.log('ref', ref);
+
   return (
     <div >
       <div>count {count}</div>
       <button onClick={handleClick}> click me</button>
 
+      <input type='text' ref={ref} />
+      <button> click </button>
     </div>
   );
 }

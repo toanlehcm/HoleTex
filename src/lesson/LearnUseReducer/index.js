0f3b867d32363d74d2c4f498743e20655e9e041b
Toan Le <thanhtoanhcm1602@gmail.com>
2024-06-14 00:58:50 +0700

    - UseReducer: demo call API.

--- src/lesson/LearnUseReducer/index.js
+++ src/lesson/LearnUseReducer/index.js
@@ -15,7 +15,10 @@ const ACTION = {
   TANG: 'TANG',
   GIAM: 'GIAM',
   XOA_HET: 'XOA_HET',
-  GAN_GIA_TRI: 'GAN_GIA_TRI'
+  GAN_GIA_TRI: 'GAN_GIA_TRI',
+  GET_USER_REQUEST: 'GET_USER_REQUEST',
+  GET_USER_SUCCESS: 'GET_USER_SUCCESS',
+  GET_USER_ERROR: 'GET_USER_ERROR',
 }
 
 const reducer = (state, action) => {
@@ -44,12 +47,72 @@ const reducer2 = (state, action) => {
   }
 }
 
+const initStateUser = { // store
+  loading: false,
+  data: [],
+  error: null
+}
+
+const usersReducer = (state, action) => {
+  switch (action.type) {
+    case ACTION.GET_USER_REQUEST:
+      return {
+        ...state,
+        loading: true
+      }
+
+    case ACTION.GET_USER_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        data: action.data // Data returned from API.
+      }
+
+    case ACTION.GET_USER_ERROR:
+      return {
+        ...state,
+        data: [],
+        err: action.data,
+      }
+
+    default:
+      return state;
+  }
+}
+
 function LearnUseReducer(props) {
   const [count, dispatch] = useReducer(reducer, 0);
   const [count2, dispatch2] = useReducer(reducer2, 0);
+  const [users, usersDispatch] = useReducer(usersReducer, initStateUser);
+
+  const getUsers = () => {
+    usersDispatch({
+      type: ACTION.GET_USER_REQUEST
+    })
+
+    setTimeout(() => {
+      fetch(`https://reqres.in/api/users`)
+        .then((res) => res.json())
+        .then((res) => {
+          usersDispatch({
+            type: ACTION.GET_USER_SUCCESS,
+            data: res
+          })
+        })
+        .catch((err) => {
+          usersDispatch({
+            type: ACTION.GET_USER_ERROR,
+            err: err
+          })
+        })
+    }, 2000)
+  }
 
   return (
     <div >
+      <button onClick={getUsers}>{ACTION.GET_USER_REQUEST}</button>
+      {users.loading ? <p>Loading</p> : <p>{JSON.stringify(users)}</p>}
+
       <p>Count: {count}</p>
       <button onClick={() => dispatch(ACTION.TANG)}>{ACTION.TANG}</button>
       <button onClick={() => dispatch(ACTION.GIAM)}>{ACTION.GIAM}</button>
@@ -60,7 +123,7 @@ function LearnUseReducer(props) {
         type: ACTION.GAN_GIA_TRI, // type tự đặt.
         data: 10
       })}>{ACTION.GAN_GIA_TRI}</button>
-    </div>
+    </div >
   );
 }
 

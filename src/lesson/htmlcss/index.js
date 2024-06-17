de8a3670abfb623abf0f201c0d8243b036158320
Toan Le <thanhtoanhcm1602@gmail.com>
2024-06-18 05:32:11 +0700

    - inline-flex in display flex CSS.

--- src/lesson/htmlcss/index.js
+++ src/lesson/htmlcss/index.js
@@ -4,7 +4,7 @@ import styles from './styles.module.scss'
 function LessonHTMLAndCSS(props) {
   return (
     <div>
-      <div className={styles.flex_container} >
+      {/* <div className={styles.flex_container} >
         <div className={`${styles.box} ${styles.box_1}`} ></div>
         <div className={`${styles.box} ${styles.box_2}`} ></div>
         <div className={`${styles.box} ${styles.box_3}`} ></div>
@@ -14,7 +14,25 @@ function LessonHTMLAndCSS(props) {
         <div className={`${styles.box} ${styles.box_1}`} ></div>
         <div className={`${styles.box} ${styles.box_2}`} ></div>
         <div className={`${styles.box} ${styles.box_3}`} ></div>
+      </div> */}
+
+      <div className={styles.flex_container_2}>
+        <div className={styles.flex_item}>Item 1</div>
+        <div className={styles.flex_item}>Item 2</div>
+        <div className={styles.flex_item}>Item 3</div>
       </div>
+
+      <div className={styles.inline_flex_container}>
+        <div className={styles.flex_item}>Item 1</div>
+        <div className={styles.flex_item}>Item 2</div>
+        <div className={styles.flex_item}>Item 3</div>
+      </div>
+
+      <p>This is some text before the <span className={styles.inline_flex_container}>
+        <div className={styles.flex_item}>Inline Item 1</div>
+        <div className={styles.flex_item}>Inline Item 2</div>
+        <div className={styles.flex_item}>Inline Item 3</div>
+      </span> and this is some text after.</p>
     </div>
   );
 }

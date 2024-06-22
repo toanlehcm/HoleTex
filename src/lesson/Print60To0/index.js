a874843bf278930910c305705eb16e5daf19790d
Toan Le <thanhtoanhcm1602@gmail.com>
2024-06-22 16:05:15 +0700

    - setTimeout from 60 to 0.

--- src/lesson/Print60To0/index.js
+++ src/lesson/Print60To0/index.js
@@ -1,38 +1,75 @@
-import React, { useMemo, useState } from 'react';
+import React, { useEffect, useMemo, useRef, useState } from 'react';
 
-const getArrayNumber = () => {
-  const arrayLoop = []
+// const getArrayNumber = () => {
+//   const arrayLoop = []
 
-  console.log('loop');
-  for (let index = 60; index >= 0; index--) {
-    arrayLoop.push(index)
-  }
+//   console.log('loop');
+//   for (let index = 60; index >= 0; index--) {
+//     arrayLoop.push(index)
+//   }
 
-  return arrayLoop
-}
+//   return arrayLoop
+// }
 
 function Print60To0(props) {
-  const [count, setCount] = useState(0)
-  const [arrayNumber, setArrayNumber] = useState(() => {
-    return getArrayNumber()
-  })
+  const [count, setCount] = useState(3)
+  const [count2, setCount2] = useState(3)
 
-  const arrayNumber2 = useMemo(() => getArrayNumber(), [])
+  // const [arrayNumber, setArrayNumber] = useState(() => {
+  //   return getArrayNumber()
+  // })
+
+  // const arrayNumber2 = useMemo(() => getArrayNumber(), [])
   // console.log('arrayNumber2', arrayNumber2);
+
+  /*------------ setTimeout ----------------*/
+  useEffect(() => {
+    if (count === 0) {
+      // console.log('return ');
+      return
+    }
+
+    const setTimeoutCount = setTimeout(() => {
+      setCount(count - 1)
+    }, 1000)
+
+    return () => { clearTimeout(setTimeoutCount) }
+  }, [count])
+
+  /*------------ setInterval ----------------*/
+  useEffect(() => {
+    const setIntervalCount = setInterval(() => {
+      setCount2((prev) => {
+        if (prev === 0) {
+          // console.log('return 2');
+          clearInterval(setIntervalCount)
+          return prev
+        }
+
+        return prev - 1
+      })
+    }, 1000)
+
+    return () => { clearInterval(setIntervalCount) }
+  }, [])
+
+
 
   return (
     <div>
       Print 60 To 0
       <p>Count: {count}</p>
-      <button onClick={() => setCount(count + 1)}>click</button>
+      <p>Count 2: {count2}</p>
 
-      <p>
+      {/* <button onClick={() => setCount(count + 1)}>click</button>  */}
+
+      {/* <p>
         {arrayNumber.map((item, index) => (<span key={index}>{item}, </span>))}
       </p>
 
       <p>
         {arrayNumber2.map((item, index) => (<span key={index}>{item}, </span>))}
-      </p>
+      </p> */}
     </div>
   );
 }
